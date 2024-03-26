ed38c22480060fc3b17fd4786b3d73737bf03b37
Felipe Rodrigues <feliperodriguesrs07@outlook.com>
2024-03-26 16:44:19 -0300

    Controllers & Testes E2E - Test Environment  Prisma

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,5 +1,31 @@
 // test enviroment consegue mudar variaveis de ambientes apenas para testes especificos
+//configurar para banco de dados
 import type { Environment } from 'vitest'
+
+//DATABASE_URL="postgresql://docker:docker@localhost:5432/apisolid?schema=public"   substitui esse schema
+
+import 'dotenv/config'
+
+import { randomUUID } from 'node:crypto'
+import { execSync } from 'node:child_process'
+
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+function generateDatabaseURL(schema: string) {//vai receber schema
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please provide a DATABASE_URL environment variable.')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)//vai ler essa url
+
+  url.searchParams.set('schema', schema)
+
+  return url.toString()
+}
+
+
 
 export default <Environment>{
   name: 'custom',
@@ -18,10 +44,21 @@ export default <Environment>{
     }
   },
   setup() {//qual código quero executar antes do meus arquivos de teste
-    console.log('Setup')
+    const schema = randomUUID() //nome do meu schema
+    const databaseURL = generateDatabaseURL(schema)
+
+    process.env.DATABASE_URL = databaseURL
+
+    execSync('npx prisma migrate deploy')//essa função é tudo o que executo no meu terminal, dploy pula a etapa de da fezer uma nova migration
+
+
     return {
-      teardown() {
-       
+      async  teardown() { //quando finaliza, apagar o schema se ele existir
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+        )
+
+        await prisma.$disconnect()//disconect com o banco de dados
       }
     }
   }
