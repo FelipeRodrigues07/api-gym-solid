5d84b38fac92ba22d354abeb0ea97b6b7c112750
Felipe Rodrigues <feliperodriguesrs07@outlook.com>
2024-03-27 16:23:59 -0300

    Controllers & E2E Tests - Other Gym Controllers

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -2,9 +2,14 @@ import { FastifyInstance } from 'fastify'
 
 import { verifyJwt } from '@/http/middlewares/verify-jwt'
 import { create } from '@/http/controllers/gyms/create'
+import { nearby } from '@/http/controllers/gyms/nearby'
+import { search } from '@/http/controllers/gyms/search'
 
 export async function gymsRoutes(app: FastifyInstance) {
     app.addHook('onRequest', verifyJwt)//todas as rotas que tiver daqui para baixo, vão chamar nosso middlewares, somente usuarios autenticados
 
-    app.post('/create', create)
+    app.post('/gyms/search', search)
+    app.post('/gyms/nearby', nearby)
+
+    app.post('/gyms', create)
   }
