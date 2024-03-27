2f61e903b5486dfaa6bbf72f78e43c7fa568d42d
Felipe Rodrigues <feliperodriguesrs07@outlook.com>
2024-03-27 10:54:01 -0300

    Controllers & Testes E2E - Test E2E authenticate

--- src/http/controllers/authenticate.spec.ts
+++ src/http/controllers/authenticate.spec.ts
@@ -1,3 +1,33 @@
-import { test } from 'vitest'   //antes de executar usa  o ambiente, viteste-enviroment-prisma
+  //antes de executar usa  o ambiente, viteste-enviroment-prisma
 
-test('ok', () => {})
+  import request from 'supertest'
+  import { app } from '@/app'
+  import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+  describe('Authenticate (e2e)', () => {
+    beforeAll(async () => {
+      await app.ready()
+    })
+  
+    afterAll(async () => {
+      await app.close()
+    })
+  
+    it('should be able to authenticate', async () => {
+      await request(app.server).post('/users').send({
+        name: 'John Doe',
+        email: 'johndoe@example.com',
+        password: '123456',
+      })
+  
+      const response = await request(app.server).post('/sessions').send({
+        email: 'johndoe@example.com',
+        password: '123456',
+      })
+  
+      expect(response.status).toEqual(200)
+      expect(response.body).toEqual({
+        token: expect.any(String),
+      })
+    })
+  })
