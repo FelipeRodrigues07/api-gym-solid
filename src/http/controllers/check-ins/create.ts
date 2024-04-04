d90c0966277b6cf0bddd58ac62fc7d23c1811295
Felipe Rodrigues <feliperodriguesrs07@outlook.com>
2024-04-04 15:38:53 -0300

    Adding custom errors in the controllers

--- src/http/controllers/check-ins/create.ts
+++ src/http/controllers/check-ins/create.ts
@@ -1,6 +1,8 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
 import { makeCheckInUseCase } from '@/use-cases/factories/make-check-in-use.case'
+import { MaxDistanceError } from '@/use-cases/errors/max-distance-error'
+import { MaxNumberOfCheckInsError } from '@/use-cases/errors/max-number-of-check-ins-error'
 
 export async function create(request: FastifyRequest, reply: FastifyReply) {
   const createCheckInParamsSchema = z.object({
@@ -19,14 +21,25 @@ export async function create(request: FastifyRequest, reply: FastifyReply) {
   const { gymId } = createCheckInParamsSchema.parse(request.params)//vem pelos parametros 
   const { latitude, longitude } = createCheckInBodySchema.parse(request.body)
 
-  const checkInUseCase = makeCheckInUseCase()
+  try {
+    const checkInUseCase = makeCheckInUseCase()
 
-  await checkInUseCase.execute({
-    gymId,
-    userId: request.user.sub,
-    userLatitude: latitude,
-    userLongitude: longitude,
-  })
+    await checkInUseCase.execute({
+      gymId,
+      userId: request.user.sub,
+      userLatitude: latitude,
+      userLongitude: longitude,
+    })
 
-  return reply.status(201).send()
+    return reply.status(201).send()
+  } catch (err) {
+    if (err instanceof MaxDistanceError) {  //se for um erro do erro personalizado
+      return reply.status(400).send({ message: err.message })
+    } else if (err instanceof MaxNumberOfCheckInsError) { // Tratamento para o novo erro personalizado
+      return reply.status(400).send({ message: err.message });
+    }
+
+
+    throw err
+  }
 }
--- src/http/controllers/check-ins/validate.ts
+++ src/http/controllers/check-ins/validate.ts
@@ -1,6 +1,8 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
 import { makeValidateCheckInUseCase } from '@/use-cases/factories/make-validate-check-in-use-case'
+import { LateCheckInValidationError } from '@/use-cases/errors/late-check-in-validation-error'
+import { ResourceNotFoundError } from '@/use-cases/errors/resource-not-found-error'
 
 export async function validate(request: FastifyRequest, reply: FastifyReply) {
   const validateCheckInParamsSchema = z.object({
@@ -9,11 +11,22 @@ export async function validate(request: FastifyRequest, reply: FastifyReply) {
 
   const { checkInId } = validateCheckInParamsSchema.parse(request.params)
 
-  const validateCheckInUseCase = makeValidateCheckInUseCase()
+  try {
+    const validateCheckInUseCase = makeValidateCheckInUseCase()
 
-  await validateCheckInUseCase.execute({
-    checkInId,
-  })
+    await validateCheckInUseCase.execute({
+      checkInId,
+    })
 
-  return reply.status(204).send()//resposta vazia
+    return reply.status(204).send()//resposta vazia
+  } catch (err) {
+    if (err instanceof ResourceNotFoundError) {  //se for um erro do erro personalizado
+      return reply.status(400).send({ message: err.message })
+    } else if (err instanceof LateCheckInValidationError ) { // Tratamento para o novo erro personalizado
+      return reply.status(400).send({ message: err.message });
+    }
+
+
+    throw err
+  }
 }
